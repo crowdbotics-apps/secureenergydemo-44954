181e7d2d70d02b713703707cb989b8ba3b752991
Crowdbotics <team@crowdbotics.com>
2023-12-11 16:39:15 +0000

    Initial commit

---
+++ backend/secureenergydemo_44954/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for secureenergydemo_44954 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "secureenergydemo_44954.settings"
+)
+
+application = get_wsgi_application()
